7df367cf468a68a10e9309fd093feafb85303e5a
Xynydomega <xynydomega@gmail.com>
2026-09-02 18:04:47 +0100

    fix unused Contact import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+
 
 function App() {
     return (
@@ -10,7 +10,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
             </Routes>
         </BrowserRouter>
     );
