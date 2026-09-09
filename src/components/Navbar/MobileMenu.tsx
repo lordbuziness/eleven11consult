f7970162d2b361a91f6bec2f9dca3ba0d4006129
Xynydomega <xynydomega@gmail.com>
2026-09-09 14:55:45 +0100

    mobile tunning

--- src/components/Navbar/MobileMenu.tsx
+++ src/components/Navbar/MobileMenu.tsx
@@ -1,7 +1,7 @@
 
 import "./MobileMenu.css";
-
-import { Phone, House } from "lucide-react";
+import { ChevronDown, Phone, House } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 interface MobileMenuProps {
@@ -10,6 +10,25 @@ interface MobileMenuProps {
 }
 
 function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
+    const [aboutOpen, setAboutOpen] = useState(false);
+    const [servicesOpen, setServicesOpen] = useState(false);
+
+    const handleAboutToggle = () => {
+        setAboutOpen((prev) => !prev);
+        setServicesOpen(false);
+    };
+
+    const handleServicesToggle = () => {
+        setServicesOpen((prev) => !prev);
+        setAboutOpen(false);
+    };
+
+    const handleLinkClick = () => {
+        setAboutOpen(false);
+        setServicesOpen(false);
+        onClose();
+    };
+
     return (
         <div
             className={`mobile-menu ${
@@ -17,18 +36,121 @@ function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
             }`}
         >
             <ul className="mobile-menu__links">
-                <li>
-                    <Link to="/about" onClick={onClose}>
-                        About
-                    </Link>
+                {/* ABOUT */}
+                <li className="mobile-menu__dropdown">
+                    <button
+                        type="button"
+                        className="mobile-menu__dropdown-button"
+                        onClick={handleAboutToggle}
+                        aria-expanded={aboutOpen}
+                    >
+                        <span>About</span>
+                        <ChevronDown
+                            size={18}
+                            className={
+                                aboutOpen
+                                    ? "mobile-menu__dropdown-icon mobile-menu__dropdown-icon--open"
+                                    : "mobile-menu__dropdown-icon"
+                            }
+                        />
+                    </button>
+
+                    <div
+                        className={
+                            aboutOpen
+                                ? "mobile-menu__submenu mobile-menu__submenu--open"
+                                : "mobile-menu__submenu"
+                        }
+                    >
+                        <Link to="/about" onClick={handleLinkClick}>
+                            Our Story
+                        </Link>
+
+                        <Link to="/process" onClick={handleLinkClick}>
+                            Our Process
+                        </Link>
+
+                        <Link to="/career" onClick={handleLinkClick}>
+                            Careers
+                        </Link>
+                    </div>
                 </li>
 
-                <li>
-                    <Link to="/services" onClick={onClose}>
-                        Services
-                    </Link>
+                {/* SERVICES */}
+                <li className="mobile-menu__dropdown">
+                    <button
+                        type="button"
+                        className="mobile-menu__dropdown-button"
+                        onClick={handleServicesToggle}
+                        aria-expanded={servicesOpen}
+                    >
+                        <span>Services</span>
+                        <ChevronDown
+                            size={18}
+                            className={
+                                servicesOpen
+                                    ? "mobile-menu__dropdown-icon mobile-menu__dropdown-icon--open"
+                                    : "mobile-menu__dropdown-icon"
+                            }
+                        />
+                    </button>
+
+                    <div
+                        className={
+                            servicesOpen
+                                ? "mobile-menu__submenu mobile-menu__submenu--open"
+                                : "mobile-menu__submenu"
+                        }
+                    >
+                        <Link to="/services" onClick={handleLinkClick}>
+                            All Services
+                        </Link>
+
+                        <Link
+                            to="/services/media"
+                            onClick={handleLinkClick}
+                        >
+                            Media & Communications
+                        </Link>
+
+                        <Link
+                            to="/services/technology"
+                            onClick={handleLinkClick}
+                        >
+                            Technology
+                        </Link>
+
+                        <Link
+                            to="/services/energy"
+                            onClick={handleLinkClick}
+                        >
+                            Energy
+                        </Link>
+
+                        <Link
+                            to="/services/agriculture"
+                            onClick={handleLinkClick}
+                        >
+                            Agriculture
+                        </Link>
+
+                        <Link
+                            to="/services/construction"
+                            onClick={handleLinkClick}
+                        >
+                            Construction
+                        </Link>
+
+                        <Link
+                            to="/services/training"
+                            onClick={handleLinkClick}
+                        >
+                            Training
+                        </Link>
+                    </div>
                 </li>
 
+                {/* PROJECTS */}
                 <li>
                     <a
                         href="https://bloomtown.eleven11consult.com/"
@@ -40,47 +162,51 @@ function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
                     </a>
                 </li>
 
+                {/* INSIGHTS */}
                 <li>
-                    <Link to="/insights" onClick={onClose}>
+                    <Link to="/insights" onClick={handleLinkClick}>
                         Insights
                     </Link>
                 </li>
 
+                {/* WORKS */}
                 <li>
-                    <Link to="/works" onClick={onClose}>
+                    <Link to="/works" onClick={handleLinkClick}>
                         Works
                     </Link>
                 </li>
 
+                {/* FAQS */}
                 <li>
-                    <Link to="/faqs" onClick={onClose}>
+                    <Link to="/faqs" onClick={handleLinkClick}>
                         FAQs
                     </Link>
                 </li>
 
+                {/* CONTACT */}
                 <li>
-                    <Link to="/career" onClick={onClose}>
-                        Career
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/process" onClick={onClose}>
-                        Our Process
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/contact" onClick={onClose}>
+                    <Link to="/contact" onClick={handleLinkClick}>
                         <Phone size={18} />
                         <span>Contact</span>
                     </Link>
                 </li>
 
+                {/* HOME */}
                 <li>
-                    <Link to="/" onClick={onClose}>
+                    <Link to="/" onClick={handleLinkClick}>
                         <House size={18} />
                         <span>Home</span>
+                    </Link>
+                </li>
+
+                {/* START A CONVERSATION */}
+                <li className="mobile-menu__conversation">
+                    <Link
+                        to="/contact"
+                        onClick={handleLinkClick}
+                        className="mobile-menu__conversation-button"
+                    >
+                        Start a conversation
                     </Link>
                 </li>
             </ul>
